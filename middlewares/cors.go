aa1d5a63a3fbbcc97872e70cf21735570b64a5cb
nekowawolf <irukainu20@gmail.com>
2026-02-18 22:13:02 +0700

    chore: allow cc.nekowawolf.xyz in CORS config

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -8,17 +8,12 @@ import (
 
 var origins = []string{
     "https://nekowawolf.xyz",
+    "https://cc.nekowawolf.xyz",
     "https://www.nekowawolf.xyz",
-    "https://cmty.nekowawolf.xyz",
     "https://admin.nekowawolf.xyz",
     "https://nekowawolf.github.io",
     "https://airdrop.nekowawolf.xyz",
     "https://portfolio.nekowawolf.xyz",
-    "https://airdropv2-src.vercel.app",
-    "https://nekowawolfv2-src.vercel.app",
-    "https://admin-airdropv2-src.vercel.app",
-    "https://portfolio-src-orpin.vercel.app",
-    "https://crypto-communityv2-src.vercel.app",
 }
 
 var Cors = cors.Config{
